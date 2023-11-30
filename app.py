1cedd2526352b24b452c7ff3a2c0bcdb511b885c
Flavian92 <flavianzefi@hotmail.com>
2023-11-30 11:27:54 +0100

    update app and requirements

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 # app.py
+import psutil
 from flask import Flask
-app = Flask(__name__)
+app = Flask()
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    cpu_percent = psutil.cpu_percent
+    mem_percent = psutil.virtual_memory().percent
+    Message = None
+    if cpu_percent > 80 or mem_percent > 80:
+        Message = "High uttilization, please scale up"
+        return f"CPU uttilization: {cpu_percent} ADN mEMEORY uTTILIZATION: {mem_percent}"
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(debug=TRUE, host='0.0.0.0')
